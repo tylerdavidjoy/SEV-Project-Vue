072cbee5f6496234a4722e1961730f52b9c9fd84
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2021-02-12 18:46:01 -0600

    Got navigation pushed to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+        <v-app-bar app>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+          <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+
+        <v-navigation-drawer
+          v-model="drawer"
+          fixed
+          temporary
+        >
+        <v-list>
+            <v-list-item
+              v-for="tab in tabs"
+              v-bind:key="tab.icon"
+              link>
+              <v-list-item-icon>
+                <v-icon>{{ tab.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <router-link :to="tab.path"> {{ tab.text }} </router-link>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </div>
     <router-view/>
   </div>
 </template>
@@ -30,3 +54,21 @@
   }
 }
 </style>
+<script>
+  export default {
+    data: () => ({ 
+    drawer: null,
+    tabs:[
+    {icon:'mdi-inbox-arrow-down',text:'Home', path:'/'},
+    {icon:'mdi-inbox-arrow-down',text:'Account', path:'/account'},
+    {icon:'mdi-inbox-arrow-down',text:'HouseHold', path:'/household'},
+    {icon:'mdi-inbox-arrow-down',text:'Groups', path:'/groups'},
+    {icon:'mdi-inbox-arrow-down',text:'Events', path:'/events'},
+    {icon:'mdi-inbox-arrow-down',text:'Directory', path:'/directory'},
+
+    ],
+
+    }
+    ),
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-system-bar app>
+    <!-- <v-system-bar app>
       <v-spacer></v-spacer>
 
       <v-icon>mdi-square</v-icon>
@@ -8,35 +8,7 @@
       <v-icon>mdi-circle</v-icon>
 
       <v-icon>mdi-triangle</v-icon>
-    </v-system-bar>
-
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      temporary
-    >
-    <v-list>
-        <v-list-item
-          v-for="tab in tabs"
-          v-bind:key="tab.icon"
-          link>
-          <v-list-item-icon>
-            <v-icon>{{ tab.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <router-link :to="tab.path"> {{ tab.text }} </router-link>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <!--  -->
-    </v-navigation-drawer>
+    </v-system-bar> -->
 
     <v-main class="grey lighten-2">
       <v-container>
@@ -75,8 +47,7 @@
     {icon:'mdi-inbox-arrow-down',text:'HouseHold', path:'/household'},
     {icon:'mdi-inbox-arrow-down',text:'Groups', path:'/groups'},
     {icon:'mdi-inbox-arrow-down',text:'Events', path:'/events'},
-    {icon:'mdi-inbox-arrow-down',text:'Directory', path:'/directory'},
-
+    {icon:'mdi-inbox-arrow-down',text:'Directory', path:'/directory'}
     ],
 
     }),
