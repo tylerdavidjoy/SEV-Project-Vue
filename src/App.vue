6ed66631e715a92fe51b0f076e77ea05e13d06e9
CamWMcDaniel <cameron.mcdaniel@eagles.oc.edu>
2021-02-13 18:15:54 -0600

    logout goes to log in drawer hides on login screen

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
           <v-toolbar-title class="flex text-center" style="color:maroon;">{{ $route.name }}</v-toolbar-title>
         </v-app-bar>
 
-        <v-navigation-drawer expand-on-hover="expandOnHover" mini-variant="miniVariant" app>
+        <v-navigation-drawer v-if="this.$route.name != 'Login'" expand-on-hover="expandOnHover" mini-variant="miniVariant" app>
         <v-list>
             <v-list-item
               v-for="tab in tabs"
---
+++ src/views/Login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Wercome to Log-in</h1>
+  </div>
+</template>
