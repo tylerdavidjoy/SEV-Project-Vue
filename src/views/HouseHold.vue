27d89b1fb6fb4b211900bcc4d05758e1c22c49e5
Caleb Bozarth <caleb.matthew.bozart@eagles.oc.edu>
2021-02-19 14:56:03 -0600

    UI Layout for household page

--- src/views/HouseHold.vue
+++ src/views/HouseHold.vue
@@ -1,5 +1,148 @@
 <template>
-  <div class="household">
-    <h1>This is a HouseHold page</h1>
-  </div>
+  <v-app>
+    <v-container>
+      <div class="flex-container">
+      <div class="flex-child-small">
+        <img src="../assets/dog.jpg" class="familyImg">
+        <br />
+        <h1>HouseHold Information</h1>
+        <br />
+        <label>Address</label>
+        <v-text-field label="Family Address" disabled></v-text-field>
+        <label>Phone Number</label>
+        <v-text-field label="Family Phone Number" disabled></v-text-field>
+        <label>Email</label>
+        <v-text-field label="Family Email" disabled></v-text-field>
+        <v-btn class="ma-2" outlined large fab color="indigo">
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-btn class="ma-2" outlined large fab color="indigo">
+          <v-icon>mdi-content-save</v-icon>
+        </v-btn>
+      </div>
+      <div class="flex-child">
+        <img src="../assets/dog.jpg" class="userImg">
+        <br />
+        <h1>Family Members</h1>
+        <br />
+        <v-list class="list">
+            <v-list-item
+              v-for="member in members"
+              v-bind:key="member" class="list">
+              <v-list-item-icon>
+                <img src="../assets/dog.jpg" class="smallUserImg">
+              </v-list-item-icon>
+
+              <v-list-item-content class="large">
+                <v-list-item-title class="large">{{ member.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+      </div>
+      </div>
+    </v-container>
+  </v-app>
 </template>
+
+<script>
+
+  export default {
+  // components: { Directory },
+    data: () => ({ 
+    // members:[
+    // {icon:'mdi-home',text:'Home', path:'/'},
+    // {icon:'mdi-account',text:'Account', path:'/account'},
+    // {icon:'mdi-home-group',text:'HouseHold', path:'/household'},
+    // {icon:'mdi-account-group',text:'Groups', path:'/groups'},
+    // {icon:'mdi-calendar',text:'Events', path:'/events'},
+    // {icon:'mdi-church',text:'Directory', path:'/directory'},
+
+    // ],
+    members:[
+      {name: 'Billy Bob jr.'},
+      {name: 'Billy Bob sr.'},
+      {name: 'Sally Bob jr.'},
+      {name: 'Sally Bob sr.'},
+      {name: 'Junior Bob jr.'},
+    ],
+
+    }
+    ),
+  }
+</script>
+
+<style lang="scss">
+
+.flex-container {
+    display: flex;
+}
+
+.flex-child {
+    flex: 1;
+    padding: 15px;
+}  
+
+.flex-child-small{
+    flex: 0.4;
+    width: 30%;
+    padding: 15px;
+}  
+
+.large {
+  font-size: 1.3em;
+}
+// div.rightDiv {
+// float: left;
+// text-align: left;
+// width: 49%;
+// margin: 5px;
+// height: 100vh;
+// color: darkblue;
+// background-color: slategray;
+// border-radius: 10px;
+// }
+
+// div.leftDiv {
+//  float: left;
+//  text-align: left;
+//  width: 49%;
+//  margin: 5px;
+//  height: 100vh;
+//  color: darkblue;
+//  background-color: slategray;
+//  border-radius: 10px;
+// }
+
+.list {
+  // background-color: darkslategray;
+  color: white;
+}
+
+img.userImg {
+  margin: 20px;
+  // width: 10%;
+  height: 10%;
+  size: auto
+}
+
+img.smallUserImg {
+  // width: 50px;
+  height: 80px;
+  size: auto
+}
+
+img.familyImg {
+  margin: 20px;
+  // width: 40%;
+  height: 40%;
+  size: auto
+}
+
+h1 {
+  color: maroon;
+  // background-color: slategray;
+  padding: 10px;
+  text-decoration: underline;
+}
+
+</style>
