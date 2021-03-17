94c02a4f334fbee13872e4be163cac126ddacede
CamWMcDaniel <cameron.mcdaniel@eagles.oc.edu>
2021-03-17 13:57:34 -0500

    forgot to change the URL back

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -238,7 +238,7 @@
 <script>
 import axios from 'axios'
 
-const apiBaseUrl = "http://localhost:3000";
+const apiBaseUrl = "http://team2.eaglesoftwareteam.com";
 
 export default {
   name: "GroupMembers",
