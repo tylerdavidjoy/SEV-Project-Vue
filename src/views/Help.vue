58b01df892ae620a5ff88375cf641222944d15f1
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2021-04-04 10:32:52 -0500

    Got steppers set up for the first tab

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -3,25 +3,54 @@
       
     <v-main class="grey lighten-2">
       <v-container>
-        <template v-if="name != ''">
-          <div>
-            <v-sheet height="200">
-              <div class="flex text-center" style="color:maroon;">
-                Welcome {{name}}
-              </div>
-            </v-sheet>
-          </div>
-        </template>
-        <v-row>
-              <v-col v-for="tab in tabs" v-bind:key="tab.icon" :link="tab.path" cols="auto" md="4" lg="4" xl="4">
-                  <!-- <button @click="$router.push(tab.path)"> -->
-                    <v-sheet  v-on:click="$router.push(tab.path)" height="250" width="250">
-                      <div style="margin-top:5%; background-color:white;"><v-icon size="800%">{{tab.icon}}</v-icon></div>
-                      <div style="font-size:300%">{{tab.text}}</div>
-                    </v-sheet>
-                  <!-- </button> -->
-              </v-col>
-        </v-row>
+         <v-card color="gray">
+          <v-card-title class="text-center justify-center py-6">
+            <h1 class="font-weight-bold display-3 dark--text">
+              Help Page
+            </h1>
+          </v-card-title>
+      
+          <v-tabs
+            v-model="tab"
+            color="gray"
+            grow
+          >
+            <v-tab
+              v-for="item in items"
+              :key="item"
+            >
+              {{ item }}
+            </v-tab>
+          </v-tabs>
+      
+          <v-tabs-items v-model="tab">
+            <!-- The v-Tab for the Account Page -->
+            <v-tab-item>
+              <v-card color="dark" flat>
+                <v-container>
+                  <v-stepper >
+                    <v-stepper-header>
+                      <template v-for="n in content.account.steps">
+                        <v-stepper-step :key="`${n}-step`" :step="n" editable>Step {{ n }}</v-stepper-step>
+              
+                        <v-divider v-if="n !== content.account.steps" :key="n"></v-divider>
+                      </template>
+                    </v-stepper-header>
+              
+                    <v-stepper-items>
+                      <v-stepper-content v-for="n in content.account.steps" :key="`${n}-content`" :step="n">
+                        <v-card class="mb-12" color="grey lighten-1" height="200px">
+                        <v-card-title>Step {{n}}</v-card-title>
+                        <v-card-text></v-card-text>
+                        </v-card>
+                      </v-stepper-content>
+                    </v-stepper-items>
+                  </v-stepper>
+                </v-container>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-card>
       </v-container>
     </v-main>
   </v-app>
@@ -30,20 +59,20 @@
 <script>
   export default {
     mounted() {
-      console.log("Window.person: " + window.person.f_name);
-      this.name = window.person.f_name + " " + window.person.l_name;
-      
+      // console.log(this.content[0].steps)
     },
     data: () => ({
-    name:"", 
-    drawer: null,
-    tabs:[
-    {icon:'mdi-account',text:'Account', path:'/account'},
-    {icon:'mdi-home-group',text:'HouseHold', path:'/household'},
-    {icon:'mdi-account-group',text:'Groups', path:'/grouplist'},
-    {icon:'mdi-calendar',text:'Events', path:'/events'},
-    {icon:'mdi-church',text:'Directory', path:'/directory'},
-    ],
+      tab: null,
+      items: [
+      'Account','Photo & File Uploading', 'Groups', 'Events','Report Printing','Searching Directory','Administrators'
+      ],
+      itemText:[
+        'The Account page is for a person to view and edit all their information like phone number and Life Events that a congregation might like to keep track of',
+        ],
+      
+      content:{
+        account:{id:0, steps:[1,2], info:['Hello','Test'], image:'',},
+      }
     }),
   }
 </script>
