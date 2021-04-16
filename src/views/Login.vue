4afce2d80fad81e89bfb492a398929fe3b6c024d
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2021-04-16 14:12:54 -0500

    Fixed the thing cuz tyler hates me

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,6 +7,10 @@
               <v-container>
                 <v-sheet class="transparent">
                   <v-img class="rounded-lg" :class="'my-2'" contain src="https://s3.amazonaws.com/media.cloversites.com/60/600d2d44-1897-4a0e-a36b-cdbcfca6584e/gallery/slides/dc347db4-eed0-471c-a85f-d148ea80afee@2x.jpg">
+                    <!-- Used to bring balance to the force -->
+                    <v-container>
+                      <v-sheet height="450" class="transparent"/>
+                    </v-container>
                     <v-container>
                       <v-sheet class="transparent" :class="'mt-16 pt-16'">
                         <div class="g-signin2" data-onsuccess="onSignIn" data-onfailure="failed" data-theme="dark" style="margin: 2% auto; width:100px;"></div>
