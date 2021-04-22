8d841279904d200cf6c27c6d1d9f0cf9c7153d7b
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2021-04-21 20:17:07 -0500

    Updated routes

--- src/router/index.js
+++ src/router/index.js
@@ -69,7 +69,7 @@ const routes = [
   },
   {
     path: '/eventlist',
-    name: 'EventList',
+    name: 'Event List',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
